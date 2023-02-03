31e13f4d61b4be39c082e97677adf0c19bba06c0
igor.ponikarev <igor.ponikarev@demlabs.net>
2023-02-03 20:10:35 +0700

    Add impl

--- mutex_impl.go
+++ mutex_impl.go
@@ -1,5 +1,19 @@
 package contest
 
+import "sync"
+
 func New() Mutex {
-	return ... 
+	return &MutexImpl{}
+}
+
+type MutexImpl struct {
+	sync.Mutex
+}
+
+func (m *MutexImpl) LockChannel() <-chan struct{} {
+	c := make(chan struct{})
+	if m.TryLock() {
+		close(c)
+	}
+	return c
 }
