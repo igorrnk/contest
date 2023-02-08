b49599c02a0be40891adc04953cb3303694f49be
igor.ponikarev <igor.ponikarev@demlabs.net>
2023-02-08 19:35:30 +0700

    Add new implementation

--- mutex_impl.go
+++ mutex_impl.go
@@ -1,42 +1,33 @@
 package contest
 
-import "sync/atomic"
-
 func New() Mutex {
-	return &MyMutex{
-		ch: make(chan struct{}),
-	}
+	m := new(MyMutex)
+	m.ch = make(chan struct{})
+	sync := make(chan bool)
+	go m.writeToChannel(sync)
+	<-sync
+	return m
 }
 
 type MyMutex struct {
-	locked int32
-	count  int64
-	ch     chan struct{}
+	ch chan struct{}
 }
 
 func (m *MyMutex) Lock() {
-	if atomic.CompareAndSwapInt32(&m.locked, 0, 1) {
-		atomic.AddInt64(&m.count, 1)
-	} else {
-		atomic.AddInt64(&m.count, 1)
-		<-m.ch
-	}
+	<-m.ch
 }
 
 func (m *MyMutex) Unlock() {
-	if atomic.CompareAndSwapInt64(&m.count, 1, 0) {
-		atomic.AddInt32(&m.locked, -1)
-	} else {
-		atomic.AddInt64(&m.count, -1)
-		m.ch <- struct{}{}
-	}
+	sync := make(chan bool)
+	go m.writeToChannel(sync)
+	<-sync
 }
 
 func (m *MyMutex) LockChannel() <-chan struct{} {
-	ch := make(chan struct{})
-	if atomic.CompareAndSwapInt32(&m.locked, 0, 1) {
-		atomic.AddInt64(&m.count, 1)
-		close(ch)
-	}
-	return ch
+	return m.ch
+}
+
+func (m *MyMutex) writeToChannel(sync chan bool) {
+	close(sync)
+	m.ch <- struct{}{}
 }
